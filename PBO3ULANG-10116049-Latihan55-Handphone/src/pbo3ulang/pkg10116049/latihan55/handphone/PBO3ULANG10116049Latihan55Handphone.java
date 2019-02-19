e8bf4c56efc190d0b0474b5e5dd11d862d7d03d5
M Ibad Guthwara <43487906+ibadguthwara@users.noreply.github.com>
2019-02-19 20:52:19 +0700

    Add files via upload

---
+++ PBO3ULANG-10116049-Latihan55-Handphone/src/pbo3ulang/pkg10116049/latihan55/handphone/PBO3ULANG10116049Latihan55Handphone.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo3ulang.pkg10116049.latihan55.handphone;
+
+/**
+ *
+ * @author ibadguthwara
+ * NAMA              : M. Ibad Guthwara
+ * KELAS             : PBO 3 ULANG
+ * NIM               : 10116049
+ * DESKRIPSI PROGRAM :
+ */
+public class PBO3ULANG10116049Latihan55Handphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Handphone hp = new Handphone( " " , " " , " " , 0 );
+        hp.displayProduct ();
+    }
+    
+}
